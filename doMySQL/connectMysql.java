b3e6130b593d1a28cd18201e89f312c6015dad60
阵雨 <278715853@qq.com>
2016-12-04 16:08:37 +0800

    学生管理系统

---
+++ doMySQL/connectMysql.java
@@ -0,0 +1,183 @@
+package doMySQL;
+	import java.sql.Connection;
+	import java.sql.Date;
+	import java.sql.DriverManager;
+	import java.sql.ResultSet;
+	import java.sql.SQLException;
+    import java.sql.Statement;
+	import java.sql.Timestamp;
+	import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+
+	public class connectMysql {
+		protected static String dbClassName = "com.mysql.jdbc.Driver";
+		protected static String dbUrl = "jdbc:mysql://localhost:3306/stusystem";
+		protected static String dbUser = "root";
+		protected static String dbPwd = "zzy123456";
+		private static Connection conn = null;
+		private static Statement stmt=null;
+		private static ResultSet rs=null;
+		
+		private connectMysql() {
+			try {
+				if (conn == null) {
+					Class.forName(dbClassName).newInstance();
+					conn = DriverManager.getConnection(dbUrl, dbUser, dbPwd);
+				}
+				else
+					return;
+			} catch (Exception ee) {
+				ee.printStackTrace();
+			}
+
+		}
+   public static boolean check(String name, String password) {
+	           if(conn==null){
+	        	   new connectMysql();
+	             }
+	    String sql = "select *  from stu where name='" + name
+			                 + "' and sno='" + password+"'" ;
+	try {
+	      stmt = conn.createStatement();
+	} catch (SQLException e2) {
+		e2.printStackTrace();
+	}
+	try {
+		  rs = (ResultSet) stmt.executeQuery(sql);
+	} catch (SQLException e1) {
+		e1.printStackTrace();
+	}
+	try {
+		if (rs.next()) {
+			/*conn.close();*/
+			return true;
+		}
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+       
+		try {
+			conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+       }
+   private static ResultSet executeQuery(String sql) {
+	try {
+		if(conn==null)
+		new connectMysql();
+		return conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE).executeQuery(sql);
+	} catch (SQLException e) {
+		e.printStackTrace();
+		return null;
+	} finally {         /*1. ResultSet executeQuery(String sql); 执行SQL查询，并返回ResultSet 对象。
+	                    2.int executeUpdate(String sql); 可执行增，删，改，返回执行受到影响的行数。
+	                    3. boolean execute(String sql); 可执行任何SQL语句，返回一个布尔值，表示是否返回ResultSet 。*/
+	}
+}
+    private static int executeUpdate(String sql) {
+	
+	try {
+		if(conn==null)
+			new connectMysql();
+		return conn.createStatement().executeUpdate(sql);
+	} catch (SQLException e) {
+		System.out.println(e.getMessage());
+		//if(e.getMessage().equals("[Microsoft][SQLServer 2000 Driver for JDBC][SQLServer]DELETE 语句与 COLUMN REFERENCE 约束 'FK_TB_BORRO_REFERENCE_TB_BOOKI' 冲突。该冲突发生于数据库 'db_library'，表 'tb_borrow', column 'bookISBN'。"))
+			
+		return -1;
+	} finally {
+	}
+}
+
+  public static void close() {
+	try {
+		conn.close();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}finally{
+		conn = null;
+	}
+} public static int Insert(String sno,String name,String sex,String date,String tel,String identify){
+	int i=0;
+	try{
+		String sql="insert into stu(sno,name,sex,date,tel,identify) values('"+sno+"','"+name+"','"+sex+"','"+date+"','"+tel+"','"+identify+"')";
+		i=connectMysql.executeUpdate(sql);
+	}catch(Exception e){
+		e.printStackTrace();
+	}
+	return i;
+}
+
+public static String[] selectInfo(String s) {
+	String string[]=new String[6];
+	String sql = "select sno,name,sex,date,tel,identify from stu where sno = '"+s+"'";
+	ResultSet rs =  connectMysql.executeQuery(sql);
+	try {
+		while (rs.next()) {
+			string[0]=rs.getString("sno");
+			string[1]=rs.getString("name");
+			string[2]=rs.getString("sex");
+			string[3]=rs.getString("date");
+			string[4]=rs.getString("tel");
+			string[5]=rs.getString("identify");
+		}
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	
+	return string;
+}
+public static int changeit(String sno,String name,String sex,String date,String tel,String identify){
+	int i=0;
+	try{
+		String sql="UPDATE  stu set sex='"+sex+"',date='"+date+"',tel='"+tel+"',identify='"+identify+"'"+"where sno='"+sno+"'";
+		i=connectMysql.executeUpdate(sql);//executeUpdate 的返回值是一个整数，指示受影响的行数（即更新计数）。对于 CREATE TABLE 或 DROP TABLE 等不操作行的语句，executeUpdate 的返回值总为零。
+	}catch(Exception e){
+		e.printStackTrace();
+	}
+	return i;
+}
+  
+public static int Insertgrade(String s1,String s2,String s3){
+	int i=0;
+	try{
+		String sql="insert into grad(sno,cno,grade) values('"+s1+"','"+s2+"','"+s3+"')";
+		i=connectMysql.executeUpdate(sql);
+	}catch(Exception e){
+		e.printStackTrace();
+	}
+	return i;
+}  
+public static List selectbookserch(String a) {
+	List list=new ArrayList();
+	String sql = "select sno,grad.cno,cname,shuxing,ccredit,grade from grad,course where grad.cno=course.cno AND sno='"+a+"'";
+	ResultSet s = connectMysql.executeQuery(sql);
+	try {
+		while (s.next()) {
+			stuinfo stuinfo=new stuinfo();
+			stuinfo.setSno(s.getString("sno"));
+			stuinfo.setCno(s.getString("cno"));
+			stuinfo.setName(s.getString("cname"));
+			stuinfo.setShuxing(s.getString("shuxing"));
+			stuinfo.setCcredit(s.getString("ccredit"));
+			stuinfo.setGrade(s.getString("grade"));
+			list.add(stuinfo);
+		}
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	
+	return list;
+} 
+
+
+       }
---
+++ doMySQL/stuinfo.java
@@ -0,0 +1,46 @@
+package doMySQL;
+
+public class stuinfo {
+       private String sno;
+       private String cno;
+       private String name;
+       private String shuxing;
+       private String ccredit;
+       private String grade;
+	public String getSno() {
+		return sno;
+	}
+	public void setSno(String sno) {
+		this.sno = sno;
+	}
+	public String getCno() {
+		return cno;
+	}
+	public void setCno(String cno) {
+		this.cno = cno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getShuxing() {
+		return shuxing;
+	}
+	public void setShuxing(String shuxing) {
+		this.shuxing = shuxing;
+	}
+	public String getCcredit() {
+		return ccredit;
+	}
+	public void setCcredit(String ccredit) {
+		this.ccredit = ccredit;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}    
+}
